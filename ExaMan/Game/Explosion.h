44d594b5792731805d436943b3e58bef1a3e5423
AmirKarajko <amir.karajko@gmail.com>
2021-08-24 00:06:40 +0200

    Improve Explosion Header, Issue #29

--- ExaMan/Game/Explosion.h
+++ ExaMan/Game/Explosion.h
@@ -1,5 +1,35 @@
+#pragma once
 #ifndef EXPLOSION_H
 #define EXPLOSION_H
+
+#include "Game/Data/Explosion/small/smallexp0.h"
+#include "Game/Data/Explosion/small/smallexp1.h"
+#include "Game/Data/Explosion/small/smallexp2.h"
+#include "Game/Data/Explosion/small/smallexp3.h"
+sf::Texture smallexp0Texture;
+sf::Texture smallexp1Texture;
+sf::Texture smallexp2Texture;
+sf::Texture smallexp3Texture;
+#include "Game/Data/Explosion/medium/mediumexp0.h"
+#include "Game/Data/Explosion/medium/mediumexp1.h"
+#include "Game/Data/Explosion/medium/mediumexp2.h"
+#include "Game/Data/Explosion/medium/mediumexp3.h"
+sf::Texture mediumexp0Texture;
+sf::Texture mediumexp1Texture;
+sf::Texture mediumexp2Texture;
+sf::Texture mediumexp3Texture;
+
+void loadExplosionImages() {
+	smallexp0Texture.loadFromMemory(smallexp0_png, smallexp0_png_size);
+	smallexp1Texture.loadFromMemory(smallexp1_png, smallexp1_png_size);
+	smallexp2Texture.loadFromMemory(smallexp2_png, smallexp2_png_size);
+	smallexp3Texture.loadFromMemory(smallexp3_png, smallexp3_png_size);
+	mediumexp0Texture.loadFromMemory(mediumexp0_png, mediumexp0_png_size);
+	mediumexp1Texture.loadFromMemory(mediumexp1_png, mediumexp1_png_size);
+	mediumexp2Texture.loadFromMemory(mediumexp2_png, mediumexp2_png_size);
+	mediumexp3Texture.loadFromMemory(mediumexp3_png, mediumexp3_png_size);
+}
+
 class Explosion
 {
 public:
